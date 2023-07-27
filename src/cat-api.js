3560e9f47e0d1b6b4d602e8c989ea4edd10aad9b
Nazar0105 <nazar01051999@gmail.com>
2023-07-27 17:31:16 +0300

    spase ahahahhah -_-

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.headers.common["x-api-key"] = "live_ RstgcAxQvpn7adh564VAFryCjeelpV vzvWpJcPGBD1H217oN4QndjgR8B2yO XoQ7";
+axios.defaults.headers.common["x-api-key"] = "live_RstgcAxQvpn7adh564VAFryCjeelpVvzvWpJcPGBD1H217oN4QndjgR8B2yOXoQ7";
 
 export function fetchBreeds() {
   return axios.get("https://api.thecatapi.com/v1/breeds").then((response) => {
