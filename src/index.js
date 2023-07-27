6eaf9e1cd85175ea9593b3ff04402974b5368d24
Nazar0105 <nazar01051999@gmail.com>
2023-07-27 17:21:36 +0300

    fix bag 10 work

--- src/index.js
+++ src/index.js
@@ -1,45 +1,9 @@
 import { fetchBreeds, fetchCatByBreed } from "./cat-api.js";
-import SlimSelect from 'slim-select'
-// all modules
-import Notiflix from 'notiflix';
 
-// one by one
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { Report } from 'notiflix/build/notiflix-report-aio';
-import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
-import { Loading } from 'notiflix/build/notiflix-loading-aio';
-import { Block } from 'notiflix/build/notiflix-block-aio';
-
-new SlimSelect({
-  select: '#selectElement'
-})
 const breedSelect = document.querySelector(".breed-select");
 const loader = document.querySelector(".loader");
 const error = document.querySelector(".error");
 const catInfo = document.querySelector(".cat-info");
-export function fetchBreeds() {
-  return axios
-    .get("https://api.thecatapi.com/v1/breeds", {
-      params: {
-        api_key: "your_api_key_here",
-      },
-    })
-    .then((response) => {
-      return response.data;
-    });
-}
-
-export function fetchCatByBreed(breedId) {
-  return axios
-    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`, {
-      params: {
-        api_key: "your_api_key_here",
-      },
-    })
-    .then((response) => {
-      return response.data[0];
-    });
-}
 
 // Fetch and populate breeds select
 fetchBreeds()
